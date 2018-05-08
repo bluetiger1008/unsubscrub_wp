09fdd75768ad2a332e0b08735b52dd81cf649f89
iam2611008 <dragonwarrior1008@yandex.com>
2018-05-07 19:56:40 -0700

    Create landing page layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,66 @@
 @extends('layouts.app')
 
-@section('content')
-  @include('partials.page-header')
+<div class="landing-page">
+  <main class="Layout_main">
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
+    <section class="Section -center">
+      <img class="Logo -large" src="@asset('images/logo.svg')">
+    </section>
+
+    <section class="Section -center">
+      <h1 class="Title">
+        Clean up your inbox
+      </h1>
+
+      <p class="Lead">
+        Tired of spam and newsletters?<br/>
+        Easily unsubscribe from subscription<br/>
+        emails with one click.
+      </p>
+
+      <a class="FancyButton" href="demo.html">
+        Get started now
+      </a>
+    </section>
+
+    <div class="SkewBox">
+      <section class="Section">
+
+        <div class="HowBox -reverse">
+          <img alt="Email service logos"
+            src="@asset('images/illustration-services.png')"
+            srcSet="@asset('images/images/illustration-services@2x.png 2x, images/illustration-services@3x.png 3x')">
+
+          <div class="HowBox_body">
+            Sign in securely with your Google email account
+          </div>
+        </div>
+
+        <div class="HowBox">
+          <img alt="List of subscriptions" 
+            src="@asset('images/illustration-list.svg')">
+
+          <div class="HowBox_body">
+            Instantly get a list of your newsletter subscriptions
+          </div>
+        </div>
+
+        <div class="HowBox -reverse">
+          <img alt="Finger pushes button" 
+            src="@asset('images/illustration-click.svg')">
+
+          <div class="HowBox_body">
+            Unsubscribe one by one, or all of them, at the tap of a button
+          </div>
+        </div>
+
+      </section>
     </div>
-    {!! get_search_form(false) !!}
-  @endif
 
-  @while (have_posts()) @php(the_post())
-    @include('partials.content-'.get_post_type())
-  @endwhile
-
-  {!! get_the_posts_navigation() !!}
-@endsection
+    <section class="Section -center">
+      <h1 class="Title">It's free, safe, and easy</h1>
+      <p><a class="FancyButton" href="demo.html">Get started now</a></p>
+      <p><a href="#">Learn about our business model</a></p>
+    </section>
+  </main>
+</div>
