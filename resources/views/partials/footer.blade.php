c3a13e0e37f640910f9a49bc0456d27b8653bcf4
Nuttawit Panyason <nuttawitpanyason@gmail.com>
2018-05-16 10:25:11 -0700

    Update footer widget

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,25 +3,6 @@
   <div class="Footer">
     <div class="Footer_inner">
       @php(dynamic_sidebar('sidebar-footer'))
-
-      <p class="Footer_copy">© 2018 Omega Media AS</p>
-
-      <div class="Footer_sites">
-        <strong>Visit our other sites & friends</strong>
-        <ul>
-          <li><a href="">Omega Legal</a></li>
-          <li><a href="">Omega Ventures</a></li>
-          <li><a href="">Spill.no</a></li>
-          <li><a href="">Biip.no</a></li>
-          <li><a href="">Juss24</a></li>
-          <li><a href="">Catering Norge</a></li>
-          <li><a href="">Kaffeautomatkontoret</a></li>
-          <li><a href="">Sensuelt.com</a></li>
-          <li><a href="">Verdivurdering Oslo</a></li>
-          <li><a href="">Verdivurdering Bergen</a></li>
-        </ul>
-      </div>
-
     </div>
   </div>
 
