95d95f50b0cf1071b6e49b1e01e047367cc2445b
Nuttawit Panyason <nuttawitpanyason@gmail.com>
2018-05-29 03:16:01 -0700

    Add google analytics

--- app/setup.php
+++ app/setup.php
@@ -13,6 +13,7 @@ use Roots\Sage\Template\BladeProvider;
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+    wp_enqueue_script( 'sage/google_analytics.js', 'https://www.googletagmanager.com/gtag/js?id=UA-3071052-66#asyncload', null, null, true );
 }, 100);
 
 /**
---
+++ resources/assets/scripts/analytics.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-3071052-66');
